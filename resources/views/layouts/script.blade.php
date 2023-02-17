f5852f508771ab5789b40d3f6fcb49f9b6598777
krisnakumar88 <krisnakumarpandit88@gmail.com>
2023-02-18 00:42:13 +0700

    templeting

---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,42 @@
+
+    <!-- Jquery js-->
+    <script src="{{ asset('/assets/plugins/jquery/jquery.min.js') }}"></script>
+
+    <!-- Bootstrap js-->
+    <script src="{{ asset('/assets/plugins/bootstrap/js/popper.min.js') }}"></script>
+    <script src="{{ asset('/assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+
+    <!-- Perfect-scrollbar js -->
+    <script src="{{ asset('/assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+
+    <!-- Sidemenu js -->
+    <script src="{{ asset('/assets/plugins/sidemenu/sidemenu.js') }}"></script>
+
+    <!-- Sidebar js -->
+    <script src="{{ asset('/assets/plugins/sidebar/sidebar.js') }}"></script>
+
+    <!-- Select2 js-->
+    <script src="{{ asset('/assets/plugins/select2/js/select2.min.js') }}"></script>
+
+    <!-- Sticky js -->
+    <script src="{{ asset('/assets/js/sticky.js') }}"></script>
+
+    <!-- Custom js -->
+    <script src="{{ asset('/assets/js/custom.js') }}"></script>
+
+    <!-- Internal Chart.Bundle js-->
+    <script src="{{ asset('/assets/plugins/chart.js/Chart.bundle.min.js') }}"></script>
+
+    <!-- Peity js-->
+    <script src="{{ asset('/assets/plugins/peity/jquery.peity.min.js') }}"></script>
+
+    <!-- Internal Morris js -->
+    <script src="{{ asset('/assets/plugins/raphael/raphael.min.js') }}"></script>
+    <script src="{{ asset('/assets/plugins/morris.js/morris.min.js') }}"></script>
+
+    <!-- Circle Progress js-->
+    <script src="{{ asset('/assets/js/circle-progress.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/chart-circle.js') }}"></script>
+
+    <!-- Internal Dashboard js-->
+    <script src="{{ asset('/assets/js/index.js') }}"></script>
